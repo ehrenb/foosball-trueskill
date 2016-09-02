aa98bb2f21700a5e6fe34655ef1ce5d957758bb5
Tyler Shake <tshake@bbn.com>
2016-09-01 18:00:01 -0700

    moved to flask repo

--- foosball_trueskill/utils/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `foosball`.`entries` (
-  `entries_id` INT NOT NULL AUTO_INCREMENT,
-  `title` TEXT NOT NULL,
-  `text` TEXT NOT NULL,
-  PRIMARY KEY (`entries_id`),
-  UNIQUE INDEX `entries_id_UNIQUE` (`entries_id` ASC));
